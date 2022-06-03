2503e14d1110f2ffdb7c3c247bb58b1c9aed91c3
Isabel Anastasiadis <isabel@boost.co.nz>
2022-06-03 15:37:19 +1200

    feat: incremented version

--- lib/consyncful/tree/version.rb
+++ lib/consyncful/tree/version.rb
@@ -2,6 +2,6 @@
 
 module Consyncful
   module Tree
-    VERSION = "0.1.1"
+    VERSION = "0.1.2"
   end
 end
--- spec/consyncful/tree_spec.rb
+++ spec/consyncful/tree_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Consyncful::Tree do
   it "has a version number" do
     expect(Consyncful::Tree::VERSION).not_to be nil
   end
+
+  it "runs Hola" do
+    Consyncful::Tree::Hola.hi
+  end
 end
