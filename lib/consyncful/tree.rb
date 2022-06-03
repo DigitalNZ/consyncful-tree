ae3ee1adcb9db37dcbbef751d552ab2b0f3d62ad
Isabel Anastasiadis <isabel@boost.co.nz>
2022-06-03 14:07:37 +1200

    feat: init

---
+++ lib/consyncful/tree.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "tree/version"
+
+module Consyncful
+  module Tree
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/consyncful/tree_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Consyncful::Tree do
+  it "has a version number" do
+    expect(Consyncful::Tree::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
