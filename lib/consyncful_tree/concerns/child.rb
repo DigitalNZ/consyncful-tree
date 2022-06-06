a48708975c95f5b2a08ea61aaa2b75d5a6d6e0a5
Isabel Anastasiadis <isabel@boost.co.nz>
2022-06-07 11:48:35 +1200

    feat: missed the module for child concern (plus added test)

--- lib/consyncful_tree/concerns/child.rb
+++ lib/consyncful_tree/concerns/child.rb
@@ -3,15 +3,17 @@
 require "active_support/concern"
 
 module ConsyncfulTree
-  extend ActiveSupport::Concern
+  module Child
+    extend ActiveSupport::Concern
 
-  included do
-    after_save do
-      parents.map(&:touch)
-    end
+    included do
+      after_save do
+        parents.map(&:touch)
+      end
 
-    def parents
-      Consyncful::Base.where(child_model_ids: id)
+      def parents
+        Consyncful::Base.where(child_model_ids: id)
+      end
     end
   end
 end
--- spec/consyncful_tree_spec.rb
+++ spec/consyncful_tree_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe ConsyncfulTree do
   it "has a version number" do
     expect(ConsyncfulTree::VERSION).not_to be nil
   end
+
+  it "has the child concern" do
+    expect(ConsyncfulTree::Child).not_to be nil
+  end
 end
